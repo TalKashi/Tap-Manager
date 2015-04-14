2ce82fc0278b9af01bb93e0f6a598b97b16118d4
SergeOxe <sergeoxe@gmail.com>
2015-04-14 15:58:37 +0300

    Sync First day

---
+++ Assets/Scripts/ShopScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopScript : MonoBehaviour {
+
+
+
+	public void OnFansClick()
+	{
+		GameManager.s_gameManger.FansUpdate (0f);
+	}
+
+	public void OnFacilitiesClick()
+	{
+		GameManager.s_gameManger.FacilitiesUpdate (0f);
+	}
+
+	public void OnStadiumClick()
+	{
+		GameManager.s_gameManger.StadiumUpdate (0f);
+	}
+
+}
