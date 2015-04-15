d7738049cb95e4c054ea0fb91a39651922dcf814
SergeOxe <sergeoxe@gmail.com>
2015-04-15 22:14:25 +0300

    Change Scene Prefab + shop scene

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class ShopScript : MonoBehaviour {
 
+	public Text m_fansLevel;
+	public Text m_facilitiesLevel;
+	public Text m_stadiumLevel;
 
+	void Start()
+	{
+		m_fansLevel.text = "Fans Level" + (GameManager.s_GameManger.m_myTeam.GetFansLevel() + 1);
+		m_facilitiesLevel.text = "Facilities Level: " + (GameManager.s_GameManger.m_myTeam.GetFacilitiesLevel() + 1);
+		m_stadiumLevel.text = "Stadium Level: "+(GameManager.s_GameManger.m_myTeam.GetStadiumLevel() + 1);
+	}
 
 	public void OnFansClick()
 	{
