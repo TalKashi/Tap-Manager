3737d71528855f0509eac7c03a0659ef7746132e
SergeOxe <sergeoxe@gmail.com>
2015-04-17 17:36:50 +0300

    TeamNames

---
+++ Assets/Scripts/TeamNamesScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeamNamesScript : MonoBehaviour {
+
+	private string[] m_names = {"Turf Toe","Gunslingers","HeAteMe","Soup-A-Stars","The Cereal Killers",
+		"The Big Dawgs","Convicted Llamas","Smartinis","Bucket of Truth","Monkeys with Crayons",
+		"Here for Beer","Alco Holics","Beer","Penguins","Ninjas in Paris",
+		"Ebola Ballers","Kim Jong Worm","Kobe Wan Kenobi","D12","IDC FC"};
+
+
+	public string GetNameInIndex(int i){
+		if (i >= m_names.Length) {
+			return null;
+		}
+		return m_names[i];
+	}
+
+
+}
