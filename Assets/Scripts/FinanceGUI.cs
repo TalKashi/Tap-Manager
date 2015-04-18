7932ec4db29e39f7d46ffd0b78d8ab4698fe4c43
TalKashi <shakikashi@gmail.com>
2015-04-18 17:15:39 +0300

    Finance update GUI

--- Assets/Scripts/FinanceGUI.cs
+++ Assets/Scripts/FinanceGUI.cs
@@ -16,13 +16,13 @@ public class FinanceGUI : MonoBehaviour {
         int income = FinanceManager.s_FinanceManager.CalculateIncome(GameManager.s_GameManger.m_myTeam);
         int outcome = FinanceManager.s_FinanceManager.CalculateOutcome(GameManager.s_GameManger.m_myTeam);
 
-		m_facilitiesCost.text = "" +  FinanceManager.s_FinanceManager.GetFacilitiesCost();
-		m_stadiumCost.text = "" +  FinanceManager.s_FinanceManager.GetStadiumCost();
-		m_salary.text = "" + FinanceManager.s_FinanceManager.GetSalary();
-		m_incomeFromTickets.text = "" + FinanceManager.s_FinanceManager.GetIncomeFromTickets ();
-		m_incomeFromMerchandise.text = "" + FinanceManager.s_FinanceManager.GetIncomeFromMerchandise ();
+		m_facilitiesCost.text = "$" +  FinanceManager.s_FinanceManager.GetFacilitiesCost();
+		m_stadiumCost.text = "$" +  FinanceManager.s_FinanceManager.GetStadiumCost();
+		m_salary.text = "$" + FinanceManager.s_FinanceManager.GetSalary();
+		m_incomeFromTickets.text = "$" + FinanceManager.s_FinanceManager.GetIncomeFromTickets ();
+		m_incomeFromMerchandise.text = "$" + FinanceManager.s_FinanceManager.GetIncomeFromMerchandise ();
 
-        m_Total.text = "Total: " + (income - outcome);
+        m_Total.text = "Total: $" + (income - outcome);
         if (income < outcome)
         {
             m_Total.color = Color.red;
