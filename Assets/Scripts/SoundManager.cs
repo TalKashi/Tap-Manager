c810f766fadf3818e574c23706d3f5c679d843ce
SergeOxe <sergeoxe@gmail.com>
2015-04-18 21:48:35 +0300

    Player Scene buttons functions

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,7 +3,31 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour {
 
+	public AudioClip m_click;
+	public AudioClip m_win;
+	public AudioClip m_lose;
+	public AudioClip m_draw;
 
+
+	public void playWinSound()
+	{
+		AudioSource.PlayClipAtPoint (m_click, transform.position);
+	}
+
+	public void playLoseSound()
+	{
+		AudioSource.PlayClipAtPoint (m_lose, transform.position);
+	}
+
+	public void playDrawSound()
+	{
+		AudioSource.PlayClipAtPoint (m_draw, transform.position);
+	}
+
+	public void playClickSound()
+	{
+		AudioSource.PlayClipAtPoint (m_click, transform.position);
+	}
 
 
 }
