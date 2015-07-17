a59ce8ef371d7b4bc26eb5bcae17c07fd45a2007
SergeOxe <sergeoxe@gmail.com>
2015-07-17 17:54:14 +0300

    Club history

--- Assets/ClubHistoryScreen.cs
+++ Assets/ClubHistoryScreen.cs
@@ -5,34 +5,35 @@ using Soomla.Store;
 
 public class ClubHistoryScreen : MonoBehaviour
 {
-    public Text m_Text;
+	public Text	m_LongestWinningStreak;
+	public Text m_LongestLosingStreak;
+	public Text m_LongestUndefeatedStreak;
+	public Text m_LongestWinninglessstreak;
+
+	public Text m_TotalWins;
+	public Text m_TotalLost;
+	public Text m_TotalDraws;
+
+	public Text m_TotalGoalsScored;
+	public Text m_TotalGoalsConceded;
+	public Text m_TotalCrowd;
+	public Text m_TotalChampionships;
 
     void Start()
     {
         GamesStatistics allTimeStatistics = GameManager.s_GameManger.m_myTeam.AllTimeStatistics;
         RecordsStatistics teamRecords = GameManager.s_GameManger.m_myTeam.TeamRecords;
-
-        m_Text.text = string.Format(
-            @"Longest Winning Streak: {0}
-Longest Losing Streak: {1}
-Longest Undefeated Streak: {2}
-Longest Winning-less streak: {3}
-
-Total Wins: {4}
-Total Lost: {5}
-Total Draws: {6}
-
-Total Goals Scored: {7}
-Total Goals Conceded: {8}
-Total Crowd: {9}",
-            teamRecords.longestWinStreak, teamRecords.longestLoseStreak, teamRecords.longestUndefeatedStreak,
-            teamRecords.longestWinlessStreak,
-            allTimeStatistics.wins, allTimeStatistics.losts, allTimeStatistics.draws,
-            allTimeStatistics.goalsFor, allTimeStatistics.goalsAgainst, allTimeStatistics.crowd);
+		m_LongestWinningStreak.text = teamRecords.longestWinStreak.ToString();  
+		m_LongestLosingStreak.text = teamRecords.longestLoseStreak.ToString();
+		m_LongestUndefeatedStreak.text = teamRecords.longestUndefeatedStreak.ToString();
+		m_LongestWinninglessstreak.text = teamRecords.longestWinlessStreak.ToString();
+		m_TotalWins.text =  allTimeStatistics.wins.ToString();
+		m_TotalLost.text = allTimeStatistics.losts.ToString();
+		m_TotalDraws.text = allTimeStatistics.draws.ToString();
+		m_TotalGoalsScored.text = allTimeStatistics.goalsFor.ToString();
+		m_TotalGoalsConceded.text = allTimeStatistics.goalsAgainst.ToString();
+		m_TotalCrowd.text = allTimeStatistics.crowd.ToString();
+		//m_TotalChampionships.text = allTimeStatistics.championships().ToString();
     }
 
-    public void OnSoomlaTestClick()
-    {
-        StoreInventory.BuyItem(Store.INSTANT_TRAIN.ItemId);
-    }
 }
